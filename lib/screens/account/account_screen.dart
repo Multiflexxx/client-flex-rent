0f04598bdbfd54e7cf4182160bbe23141c445b9b
Anton Ochel <anton.ochel@sap.com>
2020-09-07 13:04:04 +0200

    changed to stateful

--- lib/screens/account/account_screen.dart
+++ lib/screens/account/account_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:rent/models/profile_options_model.dart';
 
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends StatefulWidget {
   const AccountScreen({Key key}) : super(key: key);
 
+  @override
+  _AccountScreenState createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
