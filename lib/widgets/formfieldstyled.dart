bcd4fdf3f61f3dacaf8f03102fdf074b17f01bef
tizian123 <pizzajojomgh@gmail.com>
2020-09-08 16:34:45 +0200

    Input widget with style

---
+++ lib/widgets/formfieldstyled.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class FormFieldStyled extends StatefulWidget {
+  final Widget child;
+  final hintText;
+  final obscureText;
+  final validator;
+  final helperText;
+  final icon;
+
+  FormFieldStyled(
+      {Key key,
+      this.child,
+      this.hintText,
+      this.obscureText,
+      this.validator,
+      this.helperText,
+      this.icon})
+      : super(key: key);
+
+  _FormFieldStyledState createState() => _FormFieldStyledState();
+}
+
+class _FormFieldStyledState extends State<FormFieldStyled> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+         icon: widget.icon ?? null,
+        helperStyle: TextStyle(color: Colors.white60, height: 1.25),
+        helperMaxLines: 3,
+        helperText: widget.helperText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red),
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        hintText: widget.hintText ?? '',
+        labelStyle: TextStyle(color: Colors.white),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+        hoverColor: Colors.white,
+      ),
+      obscureText: widget.obscureText ?? false,
+      validator: widget.validator,
+    );
+  }
+}
