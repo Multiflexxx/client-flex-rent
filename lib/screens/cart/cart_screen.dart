5cf58bd0f4ca921b12e647aafdd7f9058d4f4dc5
TristanMrb <trisemig@aol.com>
2020-09-09 21:06:13 +0200

    Added tabbar to rentalitem screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,24 +1,85 @@
 import 'package:flutter/material.dart';
 
-class CartScreen extends StatelessWidget {
-  const CartScreen({Key key}) : super(key: key);
+class CartScreen extends StatefulWidget {
+  CartScreen({Key key}) : super(key: key);
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  final List<String> _tabs = <String>[
+    "Ausstehende",
+    "Gemietete",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Cart")),
-        body: Align(
-            alignment: Alignment.center,
-            child: FlatButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                onPressed: () {
-                  // Navigator.push(
-                  //     context,
-                  //     new MaterialPageRoute(
-                  //         builder: (BuildContext context) =>
-                  //             new ListViewPage()));
-                },
-                child: Text("Switch Page - Comment"))));
+      body: DefaultTabController(
+        length: _tabs.length,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverSafeArea(
+                  top: false,
+                  sliver: SliverAppBar(
+                    title: const Text('Books'),
+                    floating: true,
+                    pinned: true,
+                    snap: false,
+                    primary: true,
+                    forceElevated: innerBoxIsScrolled,
+                    bottom: TabBar(
+                      tabs:
+                          _tabs.map((String name) => Tab(text: name)).toList(),
+                    ),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: _tabs.map((String name) {
+              return SafeArea(
+                top: false,
+                bottom: false,
+                child: Builder(
+                  builder: (BuildContext context) {
+                    return CustomScrollView(
+                      key: PageStorageKey<String>(name),
+                      slivers: <Widget>[
+                        SliverOverlapInjector(
+                          handle:
+                              NestedScrollView.sliverOverlapAbsorberHandleFor(
+                                  context),
+                        ),
+                        SliverPadding(
+                          padding: const EdgeInsets.all(8.0),
+                          sliver: SliverFixedExtentList(
+                            itemExtent: 60.0,
+                            delegate: SliverChildBuilderDelegate(
+                              (BuildContext context, int index) {
+                                return Container(
+                                  color: Colors.red,
+                                  child: Text('$index'),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
   }
 }
