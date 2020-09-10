62f470ed2773bf41f8d8e2cc286242d9ac5d1c63
Anton Ochel <anton.ochel@sap.com>
2020-09-10 11:55:53 +0200

    404 added

--- lib/screens/404.dart
+++ lib/screens/404.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class PageNotFound extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('404 - Page not found'),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Octicons.hubot, color: Colors.white, size: 100.0,),
+              Text('404 - Page not found', style:
+                TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.home, color: Colors.white,), onPressed: (){
+                  //Navigator.popUntil(context, RoutePredicate());
+              }, iconSize: 50.0,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/account/settings.dart
+++ lib/screens/account/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AppSettings extends StatefulWidget {
   @override
@@ -7,7 +8,35 @@ class AppSettings extends StatefulWidget {
 
 class _AppSettingsState extends State<AppSettings> {
   @override
+
+  bool Darkmode = true;
+
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Darkmode ? Colors.black : Colors.white,
+      appBar: AppBar(
+        title: Text('Einstellungen'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Darkmode'),
+                Switch(onChanged: (bool){
+                  setState(() {
+                    Darkmode = bool;
+                  });
+                }, value: Darkmode,)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
