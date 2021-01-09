cc8dc68d5ba6a1560eeedd433eb4b0d5f1e7e6d6
Anton <antonochel@web.de>
2021-01-09 17:11:45 +0100

    picture detail view update

--- lib/screens/offer/offer_picture_detail_view.dart
+++ lib/screens/offer/offer_picture_detail_view.dart
@@ -13,6 +13,53 @@ class PictureDetailView extends StatefulWidget {
 }
 
 class _PictureDetailViewState extends State<PictureDetailView> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Builder(
+        builder: (context) {
+          final double height = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              // autoPlay: false,
+            ),
+            items: widget.pictures.map((pictureLink) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: CachedNetworkImage(
+                      imageUrl: pictureLink,
+                      height: 180.0,
+                      width: 180.0,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => Icon(
+                        Icons.error,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      errorWidget: (context, url, error) => Icon(
+                        Icons.error,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,4 +115,5 @@ class _PictureDetailViewState extends State<PictureDetailView> {
       ),
     );
   }
+  */
 }
